de3263d879cb22c8ef880e0afb27ca736a6f8625
Jonas2134 <neo.stief.jon@gmail.com>
2024-11-28 01:08:27 +0100

    add header and footer

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { FeaturedProjectsComponent } from './featured-projects/featured-projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LandingPageComponent, AboutMeComponent, SkillSetComponent, FeaturedProjectsComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
