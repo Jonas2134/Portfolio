47d3ebffcfdbaedce332074c35d88c961b54f3fd
Jonas2134 <neo.stief.jon@gmail.com>
2024-12-06 14:18:31 +0100

    Add Skills

--- src/app/main-content/skill-set/skill-set.component.ts
+++ src/app/main-content/skill-set/skill-set.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  skills = [
+    { img: "html.svg", name: "HTML" },
+    { img: "css.svg", name: "CSS" },
+    { img: "javascript.svg", name: "JavaScript" },
+    { img: "material.svg", name: "Material Design" },
+    { img: "typescript.svg", name: "TypeScript" },
+    { img: "angular.svg", name: "Angular" },
+    { img: "firebase.svg", name: "Firebase" },
+    { img: "git.svg", name: "GIT" },
+    { img: "api.svg", name: "Rest-Api" },
+    { img: "scrum.svg", name: "Scrum" },
+  ];
+
 
 }
