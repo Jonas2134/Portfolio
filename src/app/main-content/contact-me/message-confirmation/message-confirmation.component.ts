b311b428c25c2b85c748575e50c0a64058166d17
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-06 22:22:32 +0100

    add translate

--- src/app/main-content/contact-me/message-confirmation/message-confirmation.component.ts
+++ src/app/main-content/contact-me/message-confirmation/message-confirmation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-message-confirmation',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './message-confirmation.component.html',
   styleUrl: './message-confirmation.component.scss'
 })
