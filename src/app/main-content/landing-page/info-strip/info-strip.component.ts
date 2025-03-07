79c9993919ecd08c8b39eab4a8e6562b48d61bc9
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-07 20:10:07 +0100

    stripe animation

--- src/app/main-content/landing-page/info-strip/info-strip.component.ts
+++ src/app/main-content/landing-page/info-strip/info-strip.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
 import { trigger, transition, animate, style } from '@angular/animations';
+import { TranslatePipe } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-info-strip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,TranslatePipe],
   templateUrl: './info-strip.component.html',
   styleUrl: './info-strip.component.scss',
   animations: [
     trigger('scrollLeft', [
       transition('* => *', [
-        style({ transform: 'translateX(130%)' }),
-        animate('10s linear', style({ transform: 'translateX(-130%)' })),
+        style({ transform: 'translateX(25%)' }),
+        animate('10s linear', style({ transform: 'translateX(-25%)' })),
       ]),
     ]),
   ],
 })
 export class InfoStripComponent {
   state = 0;
+  infos: string[] = ['landingPage.info1', 'landingPage.info2', 'landingPage.info3', 'landingPage.info4'];
 
   toggleState() {
     this.state++;
